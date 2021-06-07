b6e1f518221b9481a97ca416f9fd769c7b233902
Shashank <shashank022@gmail.com>
2021-06-07 00:09:18 -0500

    updated changes

--- src/fetchJobs.js
+++ src/fetchJobs.js
@@ -1,4 +1,28 @@
+import React, {useReducer} from 'react';
+
+const ACTIONS = {
+    MAKE_REQUEST:'make-request',
+    GET_DATA:'get-data',
+    ERROR:'error'
+}
+
+function reducer(state, action){
+    switch (action.type){
+        case ACTIONS.MAKE_REQUEST:
+            return { loading:true, jobs:[]}
+        case  ACTIONS.GET_DATA:
+            return { ...state, loading: false, jobs:action.payload.jobs}
+        case ACTIONS.ERROR:
+            return { ...state, loading: false, error: action.payload.error, jobs:[]}
+        default:
+            return state
+    }
+}
+
 export default function fetchJobs(params, page){
+    const [ state, dispatch ] = useReducer(reducer, { job:[], loading: true} )
+
+    dispatch({type:'hello', payload:{ x:3 }})
     return{
         jobs:[],
         loading:false,
